8427dd49d99b6cb8e9f04499ed959cc8895015f5
Hafiz Ilmi <hafizx45@gmail.com>
2026-09-23 20:43:10 +0800

    Update Kartu Mahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,21 @@
+package id.ac.uniska.pbo2.p01;
+
+public class KartuMahasiswa {
+    public static void main(String[] args) {
+        String nama = "Hafiz Ilmi";
+        String npm = "2410010123";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin mendalami mata kuliah PBO 2 dan bahasa pemrograman Java";
+
+        System.out.println("==============================");
+        System.out.println("KARTU MAHASISWA PBO 2");
+        System.out.println("==============================");
+        System.out.println("Nama     : " + nama);
+        System.out.println("NPM      : " + npm);
+        System.out.println("Prodi    : " + prodi);
+        System.out.println("Semester : " + semester);
+        System.out.println("Alasan   : " + alasan);
+        System.out.println("==============================");
+    }
+}
